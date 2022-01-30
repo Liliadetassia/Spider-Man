4b7ffc3df6edb7c2c2b17e5c89262003ec9a0602
Lilia de Tassia <lylyatavares@gmail.com>
2022-01-30 19:57:19 -0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+function bigImg(x)
+{
+x.style.height="64px";
+x.style.width="64px";
+}
+
+ function normalImg(x)
+{
+x.style.height="32px";
+x.style.width="32px";
+}
+</script>
+
+<img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="smiley.gif" alt="Smiley" width="32" height="32">
